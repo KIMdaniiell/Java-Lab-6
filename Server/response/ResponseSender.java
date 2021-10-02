7e0d44389316a0ecba11582b7ef43407a2781d24
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2021-10-02 03:30:17 +0300

    Add files via upload
    Добавил модуль отправки ответов клиенту

---
+++ Server/response/ResponseSender.java
@@ -0,0 +1,35 @@
+package server.response;
+
+import server.connection.Connection;
+import server.handling.Response;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class ResponseSender {
+
+    private static final int bufferSize = 4096;
+    private Response response;
+    private SocketChannel socketChannel;
+
+    public ResponseSender (Response response, Connection connection) throws IOException {
+        this.response = response;
+        this.socketChannel = connection.getSocketChannel();
+    }
+
+    public void send() throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(response);
+
+        ByteBuffer outBuffer = ByteBuffer.wrap(byteArrayOutputStream.toByteArray());
+        while (outBuffer.hasRemaining()) {
+            socketChannel.write(outBuffer);
+        }
+        outBuffer.clear();
+        response = new Response();
+    }
+}
