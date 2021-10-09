fa993f23559e2888ca93d8c0ad36c275b3bbe5c1
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2021-10-09 09:43:32 +0300

    Add files via upload

--- Server/ServerMain.java
+++ Server/ServerMain.java
@@ -21,7 +21,7 @@ public class ServerMain {
      */
     public static void main(String[] args) {
 
-        //final int port = 40748;
+        int port = 40748;
         String sport = System.getenv("SPORT");                          // default value is "40748"
         String datapath = System.getenv("DPATH");                       // default value is "inputdata.xml"
 
@@ -30,50 +30,51 @@ public class ServerMain {
         } else if (sport == null){
             System.out.println("Error: Environmental variable [SPORT] not found.");
         } else {
-            int port = Integer.parseInt(sport);
-            Stack<MusicBand> collection = Parser.serialize(datapath);
-
-            Runtime.getRuntime().addShutdownHook(new Thread( () -> {
-                Parser.deserialize(datapath,collection);
-                System.out.println("Ой, сохранение...");
-            }));
-
-            RequestHandler requestHandler = new RequestHandler(datapath, collection);
-            RequestReciever requestReciever;
-            ResponseSender responseSender = new ResponseSender();
-
-            try (Connection connection = new Connection(port)) {
-                while (true) {
-                    try (SocketChannel socketChannel = connection.getSocketChannel()) {
-
-                        requestReciever = new RequestReciever(socketChannel);
-
-                        RequestWrapper requestWrapper = requestReciever.getRequestWrapper();
-                        Response response = requestHandler
-                                .execute(requestWrapper.getCommand(), requestWrapper.getArg(), requestWrapper.getMusicBand());
-                        responseSender.send(response, socketChannel);
-
-                    } catch (ClientClosedExeption e) {
-                        System.out.println("Error: Client is not available.");
-                        //e.printStackTrace();
-                    } catch (ClassNotFoundException e) {
-                        System.out.println("Error: Deserialization failed.");
-                        //e.printStackTrace();
-                    } catch (IOException e) {
-                        System.out.println("Error: Connection interrupted.");
-                        e.printStackTrace();
-                    }
-                }
-
-            } catch (BindException e) {
-                System.out.println("Error: The chosen port is already in use.");
-                //e.printStackTrace();
-            } catch (IllegalArgumentException e) {
+            port = Integer.parseInt(sport);
+            if ((port<0)||(port>65535)) {
                 System.out.println("Error: Port parameter is outside the specified range of valid port values.");
-                //e.printStackTrace();
-            } catch (IOException e) {
-                System.out.println("Error: IO exception. Connection failed.");
-                e.printStackTrace();
+            } else {
+                Stack<MusicBand> collection = Parser.serialize(datapath);
+
+                Runtime.getRuntime().addShutdownHook(new Thread( () -> {
+                    Parser.deserialize(datapath,collection);
+                    System.out.println("Ой, сохранение...");
+                }));
+
+                RequestHandler requestHandler = new RequestHandler(datapath, collection);
+                RequestReciever requestReciever;
+                ResponseSender responseSender = new ResponseSender();
+
+                try (Connection connection = new Connection(port)) {
+                    while (true) {
+                        try (SocketChannel socketChannel = connection.getSocketChannel()) {
+
+                            requestReciever = new RequestReciever(socketChannel);
+
+                            RequestWrapper requestWrapper = requestReciever.getRequestWrapper();
+                            Response response = requestHandler
+                                    .execute(requestWrapper.getCommand(), requestWrapper.getArg(), requestWrapper.getMusicBand());
+                            responseSender.send(response, socketChannel);
+
+                        } catch (ClientClosedExeption e) {
+                            System.out.println("Error: Client is not available.");
+                            //e.printStackTrace();
+                        } catch (ClassNotFoundException e) {
+                            System.out.println("Error: Deserialization failed.");
+                            //e.printStackTrace();
+                        } catch (IOException e) {
+                            System.out.println("Error: Connection interrupted.");
+                            e.printStackTrace();
+                        }
+                    }
+
+                } catch (BindException e) {
+                    System.out.println("Error: The chosen port is already in use.");
+                    //e.printStackTrace();
+                } catch (IOException e) {
+                    System.out.println("Error: IO exception. Connection failed.");
+                    e.printStackTrace();
+                }
             }
         }
     }
